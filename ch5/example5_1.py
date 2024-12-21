16027d37964f8cbf420cf9143abfa4c13ba3cac0
Seth Mwebi <sethmwebi27@gmail.com>
2024-12-21 09:25:49 +0300

    dataclasses, namedtuple, typing

---
+++ ch5/example5_1.py
@@ -0,0 +1,4 @@
+class Coordinate:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
---
+++ ch5/example5_2.py
@@ -0,0 +1,11 @@
+from typing import NamedTuple
+
+
+class Coordinate(NamedTuple):
+    lat: float
+    lon: float
+
+    def __str__(self):
+        ns = "N" if self.lat >= 0 else "S"
+        we = "E" if self.lon >= 0 else "W"
+        return f'{abs(self.lat):.1f}"{ns}, {abs(self.lon):.1f}"{we}'
